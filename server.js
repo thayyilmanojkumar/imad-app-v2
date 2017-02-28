521a78d9ed6f9223f999643c9b151fa997dace90
Manojkumar.T <thayyilmanojkumar@gmail.com>
2017-02-28 19:53:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article_one', function (req, res) {
-  res.send('Article One requested and will be served here');
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article_two', function (req, res) {
